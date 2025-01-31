6f73b36fc61fcb4bf15c377469bdddac0b0c1926
Mehul Suthar <mehulsuthar858@gmail.com>
2025-01-31 17:04:24 +0530

    add get students list

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -81,3 +81,17 @@ func GetById(storage storage.Storage) http.HandlerFunc {
 
 	}
 }
+
+func GetList(storage storage.Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("Fetching All Users")
+
+		students, err := storage.GetStudents()
+		if err != nil {
+			response.WriteJson(w, http.StatusInternalServerError, response.GeneralError(err))
+			return
+		}
+
+		response.WriteJson(w, http.StatusOK, students)
+	}
+}
